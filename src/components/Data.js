b288e28364a1da35b3c889c91c0accd073e0ac67
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2022-07-31 11:55:38 -0500

    Added when I graduate because it wasn't there :P

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: "About",
-    body: "I am a student at the University of illinois at Chicago. I started my journey through game developing, earning more than 2.5 million visits. Having learned event driven programming, I now have an interest in full stack development.",
+    body: "I am a student at the University of illinois at Chicago, graduating in the Fall of 2023. I started my journey through game developing, earning more than 2.5 million visits. Having learned event driven programming, I now have an interest in full stack development.",
   },
   {
     title: "Interests",
