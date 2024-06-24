f7ed24d4014105ebd0e9cf479d0a0bb2ad7f3584
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-06-24 17:40:46 -0500

    styling updates and information update

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,10 +1,10 @@
 export default [
   {
     title: "About",
-    body: "I am a student at the University of illinois at Chicago, graduating in the Fall of 2023. I started my journey through game developing, earning more than 2.5 million visits. Having learned event driven programming, I now have an interest in full stack development.",
+    body: "I currently work at Blue Cross Blue Shield as an Associate Software Developer. I went to the University of Illinois at Chicago and have a Bachelors in Science from Computer Science.",
   },
   {
     title: "Interests",
-    body: "Game development is a long time interest of mine. Right now, I made a fullstack rock-paper-scissors game the winter of Dec/22! I love running on my free time - I've recently decreased my mile time by a minute, cooking, and watching shows such as One Piece. I love cooking as it requires a set sequence of instructions, that when done right, make something great! ",
+    body: "Creating things is a long time interest of mine. I'm currently working with Microcontrollers trying to create projects! I've made a few websites such as a multiplayer online Rock Paper Scissors game. My projects are on my Github.",
   },
 ];
